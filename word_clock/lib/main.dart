264d1da44cbdc3eabacf0e5809df3177d4490145
gabriele <gabri.ardi@gmail.com>
2019-12-11 22:41:11 +0100

    Fix device orientation

--- word_clock/lib/main.dart
+++ word_clock/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_clock_helper/customizer.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'word_clock.dart';
 
@@ -16,6 +17,8 @@ void main() {
     // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override.
     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   }
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
 
   runApp(ClockCustomizer((ClockModel model) => WordClock(model)));
 }
