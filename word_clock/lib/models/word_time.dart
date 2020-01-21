3df9fb6f974202b5746db208b8f6a8a9b7d25016
Gabriele Ardizzone <gabri.ardi@gmail.com>
2020-01-21 01:35:08 +0100

    Remove unnecessary constructor in word_time.dart

--- word_clock/lib/models/word_time.dart
+++ word_clock/lib/models/word_time.dart
@@ -64,19 +64,6 @@ class WordTime extends DateTime {
     "fifty-nine",
   ];
 
-  WordTime(int year,
-      [int month = 1,
-      int day = 1,
-      int hour = 0,
-      int minute = 0,
-      int second = 0,
-      int millisecond = 0,
-      int microsecond = 0])
-      : super(
-            year, month, day, hour, minute, second, millisecond, microsecond) {
-    _timeToWords();
-  }
-
   WordTime.now() : super.now() {
     _timeToWords();
   }
